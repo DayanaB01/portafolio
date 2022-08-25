9d9f44b3f6e3769f42a3c68b88b5eb026c7099da
Dayana Betancur <dahianab01@gmail.com>
2022-08-24 23:03:31 -0500

    Create form structure

--- src/components/a-atom/ComponentForm.vue
+++ src/components/a-atom/ComponentForm.vue
@@ -1,7 +1,19 @@
 <template>
-    <form action="">
-        nombre-correo-asunto
-    </form>
+    <h1>Welcome!</h1>
+    <p>I am very happy that you want comunicate with me!</p><br>
+    <div class="forms">
+      <form action="" method="post">
+        <p>Name:</p>
+          <input type="text" name="name" class="form-control"><br>
+        <p>Email:</p>
+          <input type="text" name="email" class="form-control">
+        <label for="case">Message: <br>
+          <textarea name="textarea" id="" cols="30" rows="10">
+          </textarea>
+        </label>
+        <input type="submit" title="Send" value="send">
+      </form>
+    </div>
 </template>
 
 <script lang="ts">
