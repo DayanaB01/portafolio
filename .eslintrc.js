f49e00e150a628e0cb9c23df4e50caad32126f44
Dayana <dahianab01@gmail.com>
2023-10-18 14:22:04 -0500

    comillas

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,26 +4,26 @@ module.exports = {
     node: true,
   },
   extends: [
-    "plugin:vue/vue3-essential",
-    "@vue/airbnb",
-    "@vue/typescript/recommended",
+    'plugin:vue/vue3-essential',
+    '@vue/airbnb',
+    '@vue/typescript/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "linebreak-style": [
-      "error",
-      process.env.NODE_ENV === "prod" ? "unix" : "windows",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'linebreak-style': [
+      'error',
+      process.env.NODE_ENV === 'prod' ? 'unix' : 'windows',
     ],
   },
   overrides: [
     {
       files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
       ],
       env: {
         jest: true,
